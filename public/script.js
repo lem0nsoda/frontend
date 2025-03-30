fc6e7354be60d6b797afcbdeefbbcbe8c3454192
lem0nsoda <lena.jank@htl-braunau.at>
2025-03-30 11:27:59 +0200

    login button geht & timestap request oba sunst hoid gfühlt nix

--- public/script.js
+++ public/script.js
@@ -6,7 +6,7 @@ let nextStart = null;
 
 document.addEventListener('DOMContentLoaded', function () {
     // WebSocket-Verbindung herstellen
-    socket = new WebSocket('ws://192.168.2.209:3000');
+    socket = new WebSocket('ws://192.168.2.217:3000');
     socket.onopen = () => {
         console.log('Connected to the WebSocket server');
     };
@@ -39,12 +39,10 @@ document.addEventListener('DOMContentLoaded', function () {
         }
 
         if (message.action === 'showContent') {
+            if((message.clients.includes(clientID) || message.default == true) && clientID != null){
 
-            if(message.clients.includes(clientID)){
-                console.log("absdvnsduiovho");
-                console.log(clientID);
+                console.log("play");
                 if(message.contentData.type.includes("image")){
-
                     console.log("image");
                     showImage(message);
                 } else if(message.contentData.type.includes("video")){
@@ -54,9 +52,9 @@ document.addEventListener('DOMContentLoaded', function () {
                 }
             }
         } else if (message.action === 'showClientInfo') {
-            showClientInfo();
+            showClientInfo();           
         }  else if (message.action === 'info') {
-            console.log("info " + playlistName + " s : " +nextStart);
+            
             if(message.playlistName && !playlistName)
                 playlistName = message.playlistName;
             if(message.nextStart && !nextStart)
@@ -76,7 +74,11 @@ document.addEventListener('DOMContentLoaded', function () {
     };
 
     document.getElementById('sendButton').onclick = function () {
+
         const enteredName = document.getElementById('name').value;
+
+        console.log("Available Width: " + screen.availWidth);
+        console.log("Available Height: " + screen.availHeight);
 
         if (!enteredName) {
             alert("Bitte einen ClientName eingeben!");
@@ -84,7 +86,11 @@ document.addEventListener('DOMContentLoaded', function () {
         }
 
         if (socket.readyState === WebSocket.OPEN) {
-            const message = { clientName_new: enteredName };
+            const message = { 
+                clientName_new: enteredName,
+                width: screen.availWidth,
+                height: screen.availHeight
+             };
             socket.send(JSON.stringify(message));
             console.log("Client-Name gesendet:", enteredName);
         } else {
@@ -92,8 +98,8 @@ document.addEventListener('DOMContentLoaded', function () {
         }
     };
 
-    function showClientInfo(message) {
-
+    function showClientInfo() {
+    
         console.log("Clientid & name", clientID, clientName);
 
         let content = document.querySelector("#content");
@@ -135,19 +141,17 @@ document.addEventListener('DOMContentLoaded', function () {
     }
 
     function showVideo(message) {
-
+    
         let content = document.querySelector("#content");
         content.classList.remove("hidden");
         content.innerHTML = ""; // Vorherigen Inhalt löschen
-
+    
         let video = document.createElement("video");
         video.src = message.contentData.data;
         video.autoplay = true; // Startet automatisch
         video.loop = true; // Optional: Wiederholt das Video
         video.classList.add("video-fluid");
-
+    
         content.appendChild(video);
     }
-
-
 });
--- server.js
+++ server.js
@@ -3,9 +3,10 @@ const express = require('express');
 const path = require('path');
 const http = require('http');
 const fs = require('fs');
+const mime = require('mime-types');
 
 const app = express();
-const hostname = '192.168.2.209';
+const hostname = '192.168.2.217';
 const port = 3000;
 const server = http.createServer(app);
 const wss = new WebSocket.Server({ server });
@@ -20,48 +21,91 @@ app.get('/', (req, res) => {
 
 var playClients = [];
 
+var nextPlay = null;
+var playlistData = null;
 var savedContent = [];
 var contentDuration = [];
 
-var nextPlay = null;
 
-var playlistData = null;
+var defaultPlay = null;
+var defaultPlaylistData = null;
+var playDefault = false;
+var defaultContent = [];
+var defaultContentDuration = [];
+
+var isSending = false;
+let timeoutId = null;
+var index = 0;
+
 
 wss.on('connection', async function connection(ws) {
 
     //CLIENTID
     let clientID = null;
     let clientName = null;
-    let clientStatus = null;
 
     ws.on('message', async function incoming(message) {
         try {
             let parsedMessage = JSON.parse(message);
             let clientName_new = parsedMessage.clientName_new;
+            let client_height = parsedMessage.height;
+            let client_width = parsedMessage.width;
 
             const response = await fetch(`${apiurl}/client/getBy?table=client&where=name&is=${clientName_new}`);
             if (!response.ok) throw new Error("API-Fehler ClientName");
             const data = await response.json();
 
             if (!data.length) {
-                ws.send(JSON.stringify({ status: 'error', message: 'ClientName existiert nicht' }));
-                return;
+                var client = JSON.stringify({ 
+                    name: clientName_new, 
+                    width: client_width, 
+                    height: client_height 
+                });
+
+                const responseAdd = await fetch(`${apiurl}/client/add`, {
+                    method: 'POST',
+                    headers: { 'Content-Type': 'application/json' },
+                    body: client
+                });
+
+                if (!responseAdd.ok) throw new Error("API-Fehler status update");
+                const added = await responseAdd.json();
+                console.log(added);
+
+                clientID = added.newID;
+                clientName = clientName_new;  
             }
-
-            for(let i = 0; i < data.length; i++){
-                clientID = data[i].id;
-                clientName = data[i].name;
-                clientStatus = data[i].client_status;
-
-                if (clientStatus == 0) {
-                    break;
+            else{
+                if(data[0].client_status === 0 || data[0].client_status == null){
+                    clientID = data[0].id;
+                    clientName = data[0].name;
+                }
+                else{
+                    ws.send(JSON.stringify({ status: 'error', message: 'ClientName schon vergeben' }));
+                    return;
                 }
             }
 
-            //console.log("id:" + clientID + ", name:" + clientName + ", status:" + clientStatus);
+            var updateWH = JSON.stringify({ 
+                id: clientID, 
+                width: client_width, 
+                height: client_height 
+            });
 
-            let file =  JSON.stringify({ id: clientID, client_status: 2314 });
-            //console.log(file);
+            const responseUpdate = await fetch(`${apiurl}/client/update`, {
+                method: 'POST',
+                headers: { 'Content-Type': 'application/json' },
+                body: updateWH
+            });
+
+            if (!responseUpdate.ok) throw new Error("API-Fehler status update");
+            const updated = await responseUpdate.json();
+            console.log(updated);
+
+            console.log("id:" + clientID + ", name:" + clientName );
+
+            let file =  JSON.stringify({ id: clientID, client_status: 1 });
+            console.log(file);
 
             //status update
             const response2 = await fetch(`${apiurl}/client/updateStatus`, {
@@ -71,7 +115,7 @@ wss.on('connection', async function connection(ws) {
             });
             if (!response2.ok) throw new Error("API-Fehler status update");
             const data2 = await response2.json();
-            //console.log(data2);
+            console.log(data2);
 
             ws.send(JSON.stringify({ status: 'success', clientName: clientName_new, clientID: clientID }));
             console.log(`Client ${clientName_new} verbunden.`);
@@ -101,56 +145,99 @@ wss.on('connection', async function connection(ws) {
     });
 });
 
-
-//alle unteren funktionen so aufrufen, dass sie zusammen funktionieren
+//alle unteren funktionen so aufrufen, dass sie zusammen funktionieren 
 //vorbereitungen zum starten der playlist
 function setup(){
-    fetchPlayData().then(next =>{
+    fetchPlayData().then(next => {   //play playlist data => nächste zu spielende playlist
+
         nextPlay = next;
-        wss.clients.forEach(function each(ws) {
-            ws.send(JSON.stringify({
-                action: 'info',
-                nextStart: nextPlay.start
-            }));
-        });
+        playDefault = false;
 
-        fetchPlaylistData();
-        fetchClientsData();
+        // Timestamp-Request
+        fetch('/api/timestamp')
+            .then(response => response.json())
+            .then(data => {
+                const serverTime = data.timestamp;
+                console.log("Server-Zeit: ", serverTime);
 
-        fetchContainsData(nextPlay.playlist_ID).then(contains =>{
-            saveContent(contains).then(success=>{
-                if(success){
+                const targetTime = new Date(nextPlay.start).getTime();
+                const delay = targetTime - serverTime;
+
+                // Wenn delay länger als 5min dauert -> in 1 min nochmal probieren
+                if(delay > 300000){
+                    console.log("zu lange");
+                    setTimeout(setup, 60000);
+                    nextPlay = defaultPlay;
+                    playDefault = true;
                     sendPlaylist();
+                } else{
+                    wss.clients.forEach(function each(ws) {
+                        ws.send(JSON.stringify({ 
+                            action: 'info', 
+                            nextStart: nextPlay.start
+                        }));
+                    });
+        
+                    fetchPlaylistData();        //playlistdaten der ausgewählten playlist
+                    fetchClientsData();         //clientdaten aus plays_on mit play_id von playplaylist
+        
+                    fetchContainsData(nextPlay.playlist_ID).then(contains =>{       //content aus playlistcontains
+                        saveContent(contains, savedContent, contentDuration).then(success=>{                       //dann content aus content-table holen und in array speichern
+                            if(success){
+                                sendPlaylist();                                     //playlist senden (warten zum startzeitpunkt)
+                            }
+                        })
+                    })
                 }
-            });
-        });
+                
+            })
+            .catch(error => console.error("Fehler beim Abrufen des Server-Timestamps:", error));
     });
 }
 
 //sendet playlist daten an clients wenn startzeit erreicht
 async function sendPlaylist() {
-    let index = 0;
+    // Timestamp-Request
+    const response = await fetch('/api/timestamp');
+    const data = await response.json();
+    const serverTime = data.timestamp;
 
-    //überprüfung wie lange gewartet wird
+    // Überprüfung wie lange gewartet wird
     const targetTime = new Date(nextPlay.start).getTime();
-    const delay = targetTime - Date.now();
+    let delay = targetTime - serverTime;
+
+    let playContent = savedContent;
+    let playDuration = contentDuration;
+
+    // Wenn defaultPlay gespielt wird -> 1/10 sec Verzögerung
+    if(nextPlay == defaultPlay){
+        console.log("default");
+        delay = 100;
+        playContent = defaultContent;
+        playDuration = defaultContentDuration;
+    }
+
+    console.log(delay);
 
     if (delay <= 0) {
         console.log("Zeitpunkt bereits erreicht!");
         callback();
     } else {
-        console.log(`Warten für ${delay / 1000} Sekunden...`);
         //warten bis zeitpunkt erreicht
-        setTimeout(loop, delay);
+        isSending = true;
+        index = 0;
+        if (timeoutId) clearTimeout(timeoutId); // Bereinige vorherigen Timeout
+        timeoutId = setTimeout(loop, delay);
     }
 
     function loop() {
-        if(index < savedContent.length){
+        if (!isSending) return; // Verhindert, dass mehrere Instanzen starten
 
-            console.log("why so serious");
+        if(index < playContent.length){
+            //console.log("index: " + index + ": " + playDuration[index]);
 
-            let content = savedContent[index];
-            let duration = contentDuration[index] * 1000;
+            let content = playContent[index];
+            let duration = playDuration[index] * 1000;
 
             if (!content || content == null) {
                 console.error("Kein COntent gefunden");
@@ -158,12 +245,14 @@ async function sendPlaylist() {
             }
 
             sendContentOnClients(content);
-
+            
             index ++;
 
-            setTimeout(loop, duration);
-        }
-        else{
+            if (timeoutId) clearTimeout(timeoutId); // Bereinige vorherigen Timeout
+                timeoutId = setTimeout(loop, duration);
+
+        } 
+        else {
             reset();
         }
     }
@@ -181,7 +270,7 @@ function fetchClientsData(){
         .then(data => {
             for(let i = 0; i < data.length; i++){
                 playClients[i] = data[i].client_ID;
-            }
+            }    
         })
         .catch(error => console.error('Error fetching data:', error));
 }
@@ -197,17 +286,15 @@ function fetchPlaylistData() {
             })
             .then(data => {
                 playlistData = data[0];
-
+                
                 wss.clients.forEach(function each(ws) {
                     if (ws.readyState === WebSocket.OPEN) {
-                        ws.send(JSON.stringify({
-                            action: 'info',
+                        ws.send(JSON.stringify({ 
+                            action: 'info', 
                             playlistName: playlistData.name,
                         }));
                     }
                 });
-
-
             })
             .catch(error => console.error('Error fetching data:', error));
 }
@@ -221,7 +308,7 @@ async function fetchPlayData() {
         const response = await fetch(req);
         const data = await response.json();
         let next = null;
-        //console.log(data);
+        console.log(data);
 
         data.map(play =>{
             if(!next){
@@ -229,9 +316,38 @@ async function fetchPlayData() {
                 const timestamp = new Date(dateString).getTime();
 
                 const isExpired = time > timestamp;
+                const isOlderThan7Days = (time - timestamp) > 7 * 24 * 60 * 60 * 1000;
+
+
+                //wenn älter als 7 tage -> löschen
+                if(isOlderThan7Days){
+                    console.log("older");
+                    console.log(play.id);
+                    
+                    const reqDel = apiurl + "/playlist/delete";
+                    const bodyDel = {
+                        table: 'play_playlist',
+                        id: play.id
+                    };
+
+                    fetch(reqDel, {
+                        method: 'POST',
+                        headers: { 'Content-Type': 'application/json' },
+                        body: JSON.stringify(bodyDel)
+                    })
+                        .then(response => {
+                            if (!response.ok) throw new Error('Network response was not ok');
+                            return response.json();
+                        })
+                        .then(data => {
+                            console.log(data);                            
+                        })
+                        .catch(error => console.error('Error fetching data:', error));
+                }
 
                 if(!isExpired)
                     next=play;
+
                 console.log(isExpired ? "Abgelaufen" : "Noch gültig");
             }
         });
@@ -246,7 +362,7 @@ async function fetchContainsData(id){
     try {
         const response = await fetch(`${apiurl}/playlist/getByOrder?table=playlist_contains&by=arrangement&where=playlist_ID&is=${id}`);
         const data = await response.json();
-        //console.log(data);
+        console.log(data);
         return data;
     } catch (error) {
         console.error('Error fetching Contains data:', error);
@@ -254,16 +370,16 @@ async function fetchContainsData(id){
 }
 
 //speichert die contentdaten in ein globales Array, speichert angegebene duration in array
-async function saveContent(contains) {
+async function saveContent(contains, contentArray, durationArray) {
     for(let i = 0; i < contains.length; i++){
         await fetchContentData(contains[i].content_ID).then(content =>{
-            savedContent[i] = content;
-            contentDuration[i] = contains[i].duration;
+            contentArray[i] = content;
+            durationArray[i] = contains[i].duration;
         })
     }
 
-    //console.log("content ", savedContent);
-    console.log("dur ", contentDuration);
+    console.log("content ", contentArray);
+    console.log("durattion ", durationArray);
 
     return true;
 }
@@ -273,7 +389,7 @@ async function fetchContentData(id) {
     try {
         const response = await fetch(`${apiurl}/content/getThis?id=` + id);
         const data = await response.json();
-        //console.log(data);
+        console.log(data);
         return data[0];
     } catch (error) {
         console.error('Error fetching data:', error);
@@ -285,11 +401,13 @@ function sendContentOnClients(content){
     //nachricht an script zur anzeige
     let contentAnzeigen = {
         action: 'showContent',
+        default: playDefault,
         contentData: content,
         clients: playClients
     };
 
     wss.clients.forEach(function each(ws) {
+        console.log(ws.clientID + " : " + ws.clientName);
 
         if (ws.readyState === WebSocket.OPEN) {
             ws.send(JSON.stringify(contentAnzeigen));
@@ -301,14 +419,20 @@ function sendContentOnClients(content){
 function reset(){
     console.log("reset");
 
+    if (timeoutId) clearTimeout(timeoutId); // Bereinige aktiven Timeout
+    timeoutId = null;
+
     playClients = [];
 
     savedContent = [];
     contentDuration = [];
 
     nextPlay = null;
-
+    
     playlistData = null;
+
+    isSending = false;
+    index = 0;
 
     let resetMessage = {
         action: 'reset'
@@ -325,4 +449,45 @@ function reset(){
 
 server.listen(port, hostname, () => {
     console.log(`Server läuft unter http://${hostname}:${port}/`);
+    fetchDefault()
 });
+
+
+async function fetchDefault() {
+    try {
+        const response = await fetch(`${apiurl}/playlist/getThis?table=play_playlist&id=1`);
+        const data = await response.json();
+        console.log(data);
+        defaultPlay = data[0];
+
+        fetchDefaultPlaylistData();
+        fetchDefaultContentData();
+
+    } catch (error) {
+        console.error('Error fetching data:', error);
+    }
+}
+
+async function fetchDefaultPlaylistData() {
+    let req = apiurl + "/playlist/getThis?table=playlist&id=" + defaultPlay.playlist_ID;
+
+    const response = await fetch(req);
+    if (!response.ok) throw new Error("API-Fehler ClientName");
+    const data = await response.json();
+
+    defaultPlaylistData = data[0];
+}
+
+
+function fetchDefaultContentData(){
+
+
+    fetchContainsData(defaultPlay.playlist_ID).then(contains =>{       //content aus playlistcontains
+        saveContent(contains, defaultContent, defaultContentDuration).then(success=>{                       //dann content aus content-table holen und in array speichern
+            if(success){
+                setup();                                   //playlist senden (warten zum startzeitpunkt)
+            }
+        });
+    });
+
+}
